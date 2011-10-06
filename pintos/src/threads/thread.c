fe5bc341346c70cdc712403ee4f5609a8d553c71
Brian Cain <brianccain@gmail.com>
2011-10-06 17:23:00 -0500

    Adding thread yield higher priority to thread set priority

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -439,6 +439,8 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+
+  thread_yield_to_higher_priority();
 }
 
 /* Returns the current thread’s priority. In the presence of priority donation, returns the highest (donated) priority. You need not provide any interface to allow a thread to directly modify other threads’ priorities. The priority scheduler is not used in any later project. */
